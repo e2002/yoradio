92f14f5f29385bf51f8fc920757cbd18001a66b2
e2002 <e2002@bk.ru>
2023-03-27 10:56:34 +0300

    v0.9.130

--- yoRadio/src/core/player.cpp
+++ yoRadio/src/core/player.cpp
@@ -162,7 +162,6 @@ void Player::setOutputPins(bool isPlaying) {
   if(LED_BUILTIN!=255) digitalWrite(LED_BUILTIN, LED_INVERT?!isPlaying:isPlaying);
   bool _ml = MUTE_LOCK?!MUTE_VAL:(isPlaying?!MUTE_VAL:MUTE_VAL);
   if(MUTE_PIN!=255) digitalWrite(MUTE_PIN, _ml);
-  Serial.printf("********************************** setOutputPins %d\n", isPlaying);
 }
 
 void Player::_play(uint16_t stationId) {
