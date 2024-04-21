542419a01e32c361cf0526e99d01cb506c0846bc
dasdgw <michifrank2@gmx.de>
2024-04-21 16:01:02 +0200

    fix(serial): typo in boot info

--- yoRadio/src/core/config.cpp
+++ yoRadio/src/core/config.cpp
@@ -908,7 +908,7 @@ void Config::sleepForAfter(uint16_t sf, uint16_t sa){
 
 void Config::bootInfo() {
   BOOTLOG("************************************************");
-  BOOTLOG("*               ёPadio v%s                *", YOVERSION);
+  BOOTLOG("*               ёRadio v%s                *", YOVERSION);
   BOOTLOG("************************************************");
   BOOTLOG("------------------------------------------------");
   BOOTLOG("arduino:\t%d", ARDUINO);
